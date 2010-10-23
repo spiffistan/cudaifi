0e939bf3e7d95696d89c9f1fbcd529e4c6ae7343
= <=>
2010-10-23 14:34:40 +0200

    new algo for me

--- dsp.cu
+++ dsp.cu
@@ -176,56 +176,111 @@ void catchCudaError(const char *message)
 
 } /* end extern "C" */
 
-/*
-void print_buffer8(uint8_t* buffer, int n)
-{
-	uint8_t *block1_2 = (uint8_t*)malloc(n*sizeof(uint8_t));
-	cudaMemcpy(block1_2, buffer, n*sizeof(uint8_t), cudaMemcpyDeviceToHost);
-	printf("-----------------------------------------------------\n");
-	for(int i = 0; i < n; i++) {
-			if(i % 8 == 0)
-				printf("\n");
-			printf("%5d ", block1_2[i]);
-	}
-
-	printf("\n");
-}
-
-void print_buffer32(uint32_t* buffer, int n)
-{
-	uint32_t *block1_2 = (uint32_t*)malloc(n*sizeof(uint32_t));
-	cudaMemcpy(block1_2, buffer, n*sizeof(uint32_t), cudaMemcpyDeviceToHost);
-	printf("-----------------------------------------------------\n");
-	for(int i = 0; i < n; i++) {
-			if(i % 8 == 0)
-				printf("\n");
-			printf("%5d ", block1_2[i]);
-	}
-
-	printf("\n");
-}
-*/
 ///////////////////////////////////////////////////////////////////////////////
 // CUDA KERNELS ///////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
-__constant__ int n = 64;
+__constant__ int image_stride = 16;
+__constant__ int result_stride = 64;
 
+//meant for 512 threads
+//threadDim(8,8,4);
+
+
+
+#define LENGTH 40
+
+__global__
+void cuda_me(uint8_t *original, uint8_t *reference, int stride, uint16_t *result_block, uint32_t mb_w, uint32_t mb_h, uint8_t rightEdge, uint8_t bottomEdge)
+{
+	__shared__ uint8_t ref[LENGTH*LENGTH]; // <= (40) * (40)
+	__shared__ uint8_t orig[64];
+	__shared__ uint16_t uSAD[4][64];
+	__shared__ uint16_t results[32*32];
+	//@TODO try to read reference as a 32bit in to get coalesced values on compute 1.1
+	int xyz_index = (threadIdx.y * LENGTH) + (threadIdx.z * blockDim.x)+ threadIdx.x;
+	int ref_index = (threadIdx.y * stride) + (threadIdx.z * blockDim.x)+ threadIdx.x;
+	int xy_index = (threadIdx.y * blockDim.x) + threadIdx.x;
+	//load ref
+	for(int i = 0; i < 4; i++) {
+		ref[(i*LENGTH*8)+xyz_index] = reference[(i*blockDim.y*stride)+ref_index];
+	}
+
+
+	if(!bottomEdge) {
+		ref[(32*LENGTH)+xyz_index] = reference[(32*stride)+ref_index];
+	}
+	if(!rightEdge) { //need to fill the gap on the right side, instead of horizontal read, we do vertical.
+		ref[(threadIdx.z * LENGTH * 8)+(threadIdx.y * LENGTH)+32+threadIdx.x] = reference[(threadIdx.z*8*stride)+(stride*threadIdx.y)+(32+threadIdx.x)];
+	}
+	__syncthreads();
+
+	//Load orig with 64 threads, and a special case in ref with 64 more
+	if(threadIdx.z == 0) {
+		orig[(threadIdx.y*blockDim.x)+threadIdx.x] = original[(threadIdx.y*stride)+threadIdx.x];
+	}
+	else if(threadIdx.z == 1 && !bottomEdge && !rightEdge) {
+		ref[(32*LENGTH)+32+(threadIdx.y*LENGTH)+threadIdx.x] = reference[(32*stride)+32+(threadIdx.y*stride)+threadIdx.x];
+	}
+	__syncthreads();
+	//TEST
+
+
+
+	for(int offsetY = 0; offsetY < 32; offsetY++) {
+		for(int offsetX = 0; offsetX < 8; offsetX++) {
+			//uSAD[threadIdx.z][xy_index] = abs(ref[xyz_index] - orig[xy_index]); //TODO use __usad()
+			uSAD[threadIdx.z][xy_index] = 0;
+			uSAD[threadIdx.z][xy_index] = __usad(ref[(offsetY*LENGTH)+offsetX+xyz_index], orig[xy_index], uSAD[threadIdx.z][xy_index]);
+			__syncthreads();
+			if(xy_index < 32)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+32];
+			__syncthreads();
+			if(xy_index < 16)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+16];
+			__syncthreads();
+			if(xy_index < 8)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+8];
+			__syncthreads();
+			if(xy_index < 4)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+4];
+			__syncthreads();
+			if(xy_index < 2)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+2];
+			__syncthreads();
+			if(xy_index < 1)
+				uSAD[threadIdx.z][xy_index] += uSAD[threadIdx.z][xy_index+1];
+			__syncthreads();
+			if(threadIdx.x == 0 && threadIdx.y == 0) {
+				results[(offsetY*32)+offsetX+(threadIdx.z*8)] = uSAD[threadIdx.z][0];
+			}
+			__syncthreads();
+		}
+	}
+
+	for(int i = 0; i < mb_h; i++) {
+		result_block[(i*40*8)+xyz_index] = results[(i*32*8)+(threadIdx.y*32)+(threadIdx.z*8)+threadIdx.x];
+	}
+}
+
+
+
+/*
+__constant__ int n = 64;
 __global__
 void happy_block_8x8(uint8_t *orig, uint8_t *ref, int stride, uint32_t *result_block)
 {	
 	extern __shared__ uint32_t results[];
-	int ref_index = (blockIdx.y * gridDim.x) + blockIdx.x;
-	ref = ref + (blockIdx.y * stride) + blockIdx.x;
-	int i = threadIdx.y * stride + threadIdx.x;
-	int j = blockDim.x * threadIdx.y + threadIdx.x;
+	int res_index = (blockIdx.y * gridDim.x) + blockIdx.x; //index to the result_block
+	ref = ref + (blockIdx.y * stride) + blockIdx.x; //ref image
+	int i = threadIdx.y * stride + threadIdx.x; //index to the memory in the images.
+	int j = blockDim.x * threadIdx.y + threadIdx.x; //index to the results
 	if(j < 64) {
 		results[j] = abs(orig[i] - ref[i]);
 
 	}
 
 	__syncthreads();
-
     for(int offset = 1;offset < n; offset *= 2) {
         if(j >= offset)
             results[j] += results[j - offset];
@@ -233,12 +288,12 @@ void happy_block_8x8(uint8_t *orig, uint8_t *ref, int stride, uint32_t *result_b
         __syncthreads();
     }
 
-   	//g_odata[thid] = temp[thid];
    	__syncthreads();
    	if(j == 63)
-   		result_block[ref_index] = results[j];
+   		result_block[res_index] = results[j];
 }
 
 
 
 
+*/
--- me.cu
+++ me.cu
@@ -15,7 +15,44 @@ extern "C" {
 
 /* Motion estimation for 8x8 block */
 
-void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint8_t *ref, int cc)
+void testEqual(uint16_t *results, uint8_t *ref,int width,int height, int result_stride, int ref_stride) {
+	uint16_t *res_host = (uint16_t*)malloc(height*result_stride*sizeof(uint16_t));
+	uint8_t *ref_host = (uint8_t*)malloc(height*ref_stride*sizeof(uint8_t));
+
+	cudaMemcpy(res_host,results, height*result_stride*sizeof(uint16_t), cudaMemcpyDeviceToHost);
+	cudaMemcpy(ref_host,ref, height*ref_stride*sizeof(uint8_t), cudaMemcpyDeviceToHost);
+	int x = 0, y = 0;
+	for(y = 0; y < height; y++) {
+		printf("\nROW %d= ", y);
+		for(x = 0; x < width;x++) {
+			const char *c = ref_host[y*ref_stride+x] == res_host[y*result_stride+x] ? "C" : "W";
+			printf("%s",c);
+		}
+	}
+	printf("\n");
+	free(res_host);
+	free(ref_host);
+	exit(-1);
+}
+
+
+void printResult(uint16_t *results,int width,int height, int result_stride) {
+	uint16_t *res_host = (uint16_t*)malloc(height*result_stride*sizeof(uint16_t));
+
+	cudaMemcpy(res_host,results, height*result_stride*sizeof(uint16_t), cudaMemcpyDeviceToHost);
+	int x = 0, y = 0;
+	for(y = 0; y < height; y++) {
+		printf("ROW %d= ", y);
+		for(x = 0; x < width;x++) {
+			printf("%d, ", res_host[y*result_stride+x]);
+		}
+		printf("\n");
+	}
+	free(res_host);
+}
+
+
+void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint8_t *ref, int cc,uint16_t *result_sad, uint16_t *sums)
 {
     struct macroblock *mb = &cm->curframe->mbs[cc][mb_y * cm->padw[cc]/8 + mb_x];
     int range = cm->me_search_range;
@@ -30,14 +67,20 @@ void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint
 
     /* Make sure we are within bounds of reference frame */
     // TODO: Support partial frame bounds
+    bool rightEdge = false;
+    bool bottomEdge = false;
     if (left < 0)
         left = 0;
     if (top < 0)
         top = 0;
-    if (right > (w - 8))
+    if (right > (w - 8)) {
         right = w - 8;
-    if (bottom > (h - 8))
+        rightEdge = true;
+    }
+    if (bottom > (h - 8)) {
         bottom = h - 8;
+        bottomEdge = true;
+    }
 
 
     int x,y;
@@ -47,26 +90,25 @@ void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint
     int best_sad = INT_MAX;
     int search_width = right-left;
     int search_height = bottom-top;
-    int size = search_height * search_width;
-    uint32_t *result_block1;
-    cudaMalloc((void**)&result_block1, size * sizeof(uint32_t));
+    int num_mb_width = search_width / 8;
+    int num_mb_height = search_height / 8;
 
-    dim3 blockSize(search_width,search_height,1);
-    dim3 threadSize(8,8,1);
-	happy_block_8x8<<<blockSize,threadSize,64*sizeof(uint32_t)>>>(orig + my*w+mx, ref + top*w+left, w, result_block1);
 
-	catchCudaError("FAILED happy_block_8x8");
+    dim3 threadSize(8,8,4);
+
+    cuda_me<<<1,threadSize>>>(orig + my*w+mx, ref + top*w+left, w, result_sad, num_mb_width,num_mb_height,rightEdge, bottomEdge);
+
+    catchCudaError("FAILED happy_block_8x8");
+
 	//print_buffer32(result_block1, 64*64);
-	uint32_t * sums = (uint32_t*)malloc(size * sizeof(uint32_t));
-	cudaMemcpy(sums,result_block1, size * sizeof(uint32_t), cudaMemcpyDeviceToHost);
+	cudaMemcpy(sums,result_sad, 40*40 * sizeof(uint16_t), cudaMemcpyDeviceToHost);
 	//#pragma unroll loop 64
-
 
     for (y=0; y<search_height; ++y)
     {
         for (x=0; x<search_width; ++x)
         {
-            int sad = sums[y*search_width+x];
+            int sad = sums[y*40+x];
             //sad_block_8x8(orig + my*w+mx, ref + y*w+x, w, &sad);
 
             //printf("(%4d,%4d) %d\n", x, y, sad);
@@ -80,8 +122,7 @@ void me_block_8x8(struct c63_common *cm, int mb_x, int mb_y, uint8_t *orig, uint
             }
         }
     }
-    cudaFree(result_block1);
-    free(sums);
+
     /* Here, there should be a threshold on SAD that checks if the motion vector is
      * cheaper than intraprediction. We always assume MV to be beneficial */
 
@@ -96,21 +137,22 @@ extern "C" void c63_motion_estimate(struct c63_common *cm)
 	uint8_t *image_orig, *image_ref, *image_orig_2, *image_ref_2;
 	int size = cm->width*cm->height;
 
+    uint16_t *result_sad, *sums;
+    cudaMalloc((void**)&result_sad, 40*40 * sizeof(uint16_t));
 	cudaMalloc((void**)&image_orig, size * sizeof(uint8_t));
 	cudaMalloc((void**)&image_ref, size * sizeof(uint8_t));
+	sums = (uint16_t*)malloc(40*40 * sizeof(uint16_t));
 
 	cudaMemcpy(image_orig, cm->curframe->orig->Y, size * sizeof(uint8_t), cudaMemcpyHostToDevice);
 	cudaMemcpy(image_ref, cm->refframe->recons->Y, size * sizeof(uint8_t), cudaMemcpyHostToDevice);
 
-
-	int mb_x, mb_y;
-
-    /* Luma */
+    // Luma
+	int mb_y, mb_x;
     for (mb_y=0; mb_y < cm->mb_rows; ++mb_y)
     {
         for (mb_x=0; mb_x < cm->mb_cols; ++mb_x)
         {
-            me_block_8x8(cm, mb_x, mb_y, image_orig, image_ref, 0);
+            me_block_8x8(cm, mb_x, mb_y, image_orig, image_ref, 0, result_sad, sums);
         }
     }
 
@@ -122,19 +164,22 @@ extern "C" void c63_motion_estimate(struct c63_common *cm)
 	cudaMemcpy(image_orig_2, cm->curframe->orig->V, size * sizeof(uint8_t), cudaMemcpyHostToDevice);
 	cudaMemcpy(image_ref_2, cm->refframe->recons->V, size * sizeof(uint8_t), cudaMemcpyHostToDevice);
 
-    /* Chroma */
+    // Chroma
     for (mb_y=0; mb_y < cm->mb_rows/2; ++mb_y)
     {
         for (mb_x=0; mb_x < cm->mb_cols/2; ++mb_x)
         {
-            me_block_8x8(cm, mb_x, mb_y, image_orig, image_ref, 1);
-            me_block_8x8(cm, mb_x, mb_y, image_orig_2, image_ref_2, 2);
+            me_block_8x8(cm, mb_x, mb_y, image_orig, image_ref, 1, result_sad,sums);
+            me_block_8x8(cm, mb_x, mb_y, image_orig_2, image_ref_2, 2,result_sad,sums);
         }
     }
+
     cudaFree(image_orig);
     cudaFree(image_orig_2);
     cudaFree(image_ref);
     cudaFree(image_ref_2);
+    cudaFree(result_sad);
+    free(sums);
 
 }
 
